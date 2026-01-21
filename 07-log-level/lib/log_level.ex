cb06a8f26831dfffd84ebaf512dd1ef74e3886e2
Neoplatonist <16985758+Neoplatonist@users.noreply.github.com>
2026-01-21 13:48:27 +0900

    challenge/07-log-level

---
+++ 07-log-level/lib/log_level.ex
@@ -0,0 +1,25 @@
+defmodule LogLevel do
+  @levels [:trace, :debug, :info, :warning, :error, :fatal, :unknown]
+
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 and not legacy?-> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
+    cond do
+      label == :unknown and legacy? -> :dev1
+      label == :unknown -> :dev2
+      label in [:error, :fatal] -> :ops
+      true -> false
+    end
+  end
+end
